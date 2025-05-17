5f8ef386d8f299236dcf5cbb1dd4d8f70c09325c
Aman-web-dev <amanzhx1234+github@gmail.com>
2025-05-17 22:23:33 +0530

    Created UI

---
+++ src/app/watch/page.tsx
@@ -0,0 +1,23 @@
+const page = () => {
+  return (
+    <div className="flex flex-col items-center ">
+      <h1 className="font-bold ">This Is The place to Watch Stream</h1>
+
+      <div className="bg-blue-700 w-full h-screen flex flex-col items-center">
+        <div className="h-[80%] w-full bg-black">Screen</div>
+
+        <section className="gap-4">
+          <button className="h-12 m-4 bg-white mx-auto text-black px-8 rounded-xl cursor-pointer">
+            Watch Stream
+          </button>
+
+          <button className="h-12 m-4 bg-white mx-auto text-black px-8 rounded-xl cursor-pointer">
+            End Stream
+          </button>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default page;
